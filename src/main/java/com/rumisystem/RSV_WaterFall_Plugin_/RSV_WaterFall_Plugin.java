434afc512d0b8dc3ef7c2e25d87641973b742041
SINtyanneru <irjitgd@gmail.com>
2023-12-20 18:53:09 +0900

    初期設定完

---
+++ src/main/java/com/rumisystem/RSV_WaterFall_Plugin_/RSV_WaterFall_Plugin.java
@@ -0,0 +1,16 @@
+package com.rumisystem.RSV_WaterFall_Plugin_;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class RSV_WaterFall_Plugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

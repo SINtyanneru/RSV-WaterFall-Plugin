48158ea84001eaed051381b311ecf00fc276d626
SINtyanneru <irjitgd@gmail.com>
2023-12-20 23:47:49 +0900

    とりあえずチャット関連の実装までこぎつけた

--- src/main/java/com/rumisystem/RSV_WaterFall_Plugin_/RSV_WaterFall_Plugin.java
+++ src/main/java/com/rumisystem/RSV_WaterFall_Plugin_/RSV_WaterFall_Plugin.java
@@ -3,14 +3,37 @@ package com.rumisystem.RSV_WaterFall_Plugin_;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class RSV_WaterFall_Plugin extends Plugin {
+	private static RSV_WaterFall_Plugin INSTANCE;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-    }
+	@Override
+	public void onEnable() {
+		INSTANCE = this;
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+		//プラグインの起動処理
+		getLogger().info("+----------[RWP]----------+");
+		getLogger().info("|RWP Enabled!!!           |");
+		getLogger().info("|V1.0                     |");
+		getLogger().info("+-------------------------+");
+
+		CONFIG_SYSTEM CS = new CONFIG_SYSTEM();
+		CS.CHECK();
+		CS.LOADING();
+
+		getProxy().getPluginManager().registerListener(this, new Events(CS));
+
+		//有効化？
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		//プラグインのシャットダウン処理
+		getLogger().info("RWP Disabled... GoodBye");
+
+		super.onDisable();
+	}
+
+	public static RSV_WaterFall_Plugin getInstance() {
+		return INSTANCE;
+	}
 }
